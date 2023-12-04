aefb71ec12e024c3ce03a168ea3a17ce3da18cc0
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2023-12-03 20:55:42 -0800

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -2,7 +2,7 @@
  *
  * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
  * column until a player gets four-in-a-row (horiz, vert, or diag) or until
- * board fills (tie)
+ * the board fills (tie)
  */
 
 class Game {
